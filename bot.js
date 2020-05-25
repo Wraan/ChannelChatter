bb60620209701ecb67ede23a3f957e2461a94b1c
Kamil Drewniak <zibix12@gmail.com>
2020-05-25 23:39:00 +0200

    Working bot

--- bot.js
+++ bot.js
@@ -3,66 +3,137 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const fs = require('fs');
 const request = require('request');
+const schedule = require('node-schedule');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('ChannelChatter is working!')
+    console.log('ChannelChatter is working!');
 })
 
 client.on('message', msg => {
-    let authorPrefix = `[**` + msg.author.username + `**]: `;
+    const authorUsername = msg.author.username;
 
-    if(msg.author.username === client.user.username) return;
     if(msg.channel.type !== "dm") return;
+    if(authorUsername === client.user.username) { return; }
+    else { msg.react('🖕'); }
+    if(msg.author.presence.status === 'offline') {
+        sendRequiredPresenceError(msg.author);
+        return;
+    }
+    if(msg.author.presence.member.voice.channel === null) {
+        sendNotConnectedToVoiceChannelError(msg.author);
+        return;
+    }
 
-    let attachment = msg.attachments.first();
+
+    const authorPrefix = `[**` + authorUsername + `**]: `;
+    const attachment = msg.attachments.first();
+    const content = authorPrefix + msg.content
+
+    let channelMembers = msg.author.presence.member.voice.channel.members;
+    removeAuthorFromChannelList(authorUsername, channelMembers);
+
     if(attachment) {
-        //TODO image name as a timestamp
-        creteTempDir();
-        let tempPath = `./temp/${attachment.name}`;
+        const randomName = generateRandomString(32);
+        const tempPath = `./temp/${randomName}.png`;
         
         //callback - send image right after download is finished and then delete it
-        let picStream = fs.createWriteStream(tempPath);
+        const picStream = fs.createWriteStream(tempPath);
         picStream.on('close', function() {
-            //TODO check if presence is null
-            let channelMembers = msg.author.presence.member.voice.channel.members;
-            channelMembers.forEach((member) => {
-                if(member.user.bot) return;
-            
-                member.user.createDM();
-                member.user.send(authorPrefix + msg.content, {files: [tempPath]});
-            });
-            
+            sendMessageToVoiceChannel(channelMembers, content, tempPath);
+
             setTimeout(function() {
                 fs.unlinkSync(tempPath);
-            }, 3000);
+            }, 10000); //10 seconds 
         });
 
-        downloadImage(attachment.proxyURL, picStream)
+        downloadImage(attachment.proxyURL, picStream);
     } else {
-        let channelMembers = msg.author.presence.member.voice.channel.members;
-        channelMembers.forEach((member) => {
-            if(member.user.bot) return;
-            
-            member.user.createDM();
-            member.user.send(authorPrefix + msg.content);
-        });
-
+        sendMessageToVoiceChannel(channelMembers, content);
     }
 });
 
-function downloadImage(url, picStream){
-    request.get(url)
-        .on('error', console.error)
-        .pipe(picStream);
+function scheduleTempFolderClear(){
+    const rule = new schedule.RecurrenceRule();
+    rule.hour = 4;
+
+    createTempDir();
+    let job = schedule.scheduleJob(rule, function(){
+        console.log("Cleaning temp folder");
+        removeDirContent('./temp')
+      });
 }
 
-function creteTempDir(){
-    var dir = './temp';
+function removeDirContent(dir){
+    try { var files = fs.readdirSync(dir); }
+    catch(e) { return; }
+    if (files.length > 0){
+        for (let i = 0; i < files.length; i++) {
+          let filePath = dir + '/' + files[i];
+          if (fs.statSync(filePath).isFile())
+            fs.unlinkSync(filePath);
+          else
+            removeDirContent(filePath);
+        }
+    }
+}
+
+function removeAuthorFromChannelList(authorUsername, channelMembers) {
+    let memberDel = null;
+    channelMembers.forEach((member) => {
+        if(member.user.username === authorUsername) {
+            memberDel = member;
+            return;
+        }
+    })
+    channelMembers.delete(memberDel.user.id);
+}
+
+function sendRequiredPresenceError(user) {
+    user.createDM();
+    user.send("You have to be online and visible in order to send messages to voice channel.");
+}
+
+function sendNotConnectedToVoiceChannelError(user) {
+    user.createDM();
+    user.send("You have to be connected to voice channel in order to send voice channel messages.");
+}
+
+function createTempDir(){
+    const dir = './temp';
 
     if (!fs.existsSync(dir)){
         fs.mkdirSync(dir);
     }
 }
 
+function generateRandomString(length){
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     }
+     return result;
+}
+
+function sendMessageToVoiceChannel(members, content, filePath){
+    members.forEach((member) => {
+        if(member.user.bot) return;
+        
+        member.user.createDM();
+        if(filePath)
+            member.user.send(content, {files: [filePath]});
+        else
+            member.user.send(content); 
+    });
+}
+
+function downloadImage(url, stream){
+    request.get(url)
+        .on('error', console.error)
+        .pipe(stream);
+}
+
+scheduleTempFolderClear();
 client.login(process.env.DISCORD_TOKEN);
